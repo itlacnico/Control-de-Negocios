50d541bae6de8922e1ba0be13224a08b45119c3e
Raul Moctezuma <kabal@live.com.mx>
2013-08-10 13:59:23 -0500

    DetallesTablas

--- app/cache/dev/twig/4b/8d/92a58a2152928c9f40fe9768f62e.php
+++ app/cache/dev/twig/4b/8d/92a58a2152928c9f40fe9768f62e.php
@@ -13,6 +13,7 @@ class __TwigTemplate_4b8d92a58a2152928c9f40fe9768f62e extends Twig_Template
             'title' => array($this, 'block_title'),
             'cabecera' => array($this, 'block_cabecera'),
             'content' => array($this, 'block_content'),
+            'javascripts' => array($this, 'block_javascripts'),
         );
     }
 
@@ -47,11 +48,11 @@ class __TwigTemplate_4b8d92a58a2152928c9f40fe9768f62e extends Twig_Template
     {
         // line 12
         echo "\t<div class=\"col-lg-4\">
-\t\t<ul class=\"nav nav-pills nav-stacked\" style=\"max-width: 300px;\">
-\t\t  <li class=\"active\"><a href=\"\">Operadores</a></li>
-\t\t  <li><a href=\"\">Operadores Libres</a></li>
-\t\t  <li><a href=\"\">Operadores Ocupados</a></li>
-\t\t  <li><a href=\"\">Crear Operador</a></li>
+\t\t<ul class=\"nav nav-pills nav-stacked\" style=\"max-width: 300px;\" id=\"myTab\">
+\t\t  <li class=\"active\"><a href=\"#mostrar_operadores\" data-toggle=\"tab\">Operadores</a></li>
+\t\t  <li><a href=\"\" data-toggle=\"tab\">Operadores Libres</a></li>
+\t\t  <li><a href=\"\" data-toggle=\"tab\">Operadores Ocupados</a></li>
+\t\t  <li><a href=\"#crear\" data-toggle=\"tab\">Crear Operador</a></li>
 \t\t</ul>
 
 \t\t<a href=\"";
@@ -60,32 +61,58 @@ class __TwigTemplate_4b8d92a58a2152928c9f40fe9768f62e extends Twig_Template
         echo "\"><button class=\"btn\">Crear Operador</button></a>
 \t</div>
 
+\t<div class=\"tab-content\">
+\t\t<div class=\"tab-pane\" id=\"crear\">
+\t\t\tCrear
+\t\t</div>
+\t\t<div class=\"tab-pane\" id=\"mostrar_operadores\">
+\t\t\tMostrar
+\t\t</div>
+\t</div>
+
 \t<div class=\"col-lg-8\">
-\t\t";
-        // line 24
+\t\t\t";
+        // line 33
         $context['_parent'] = (array) $context;
         $context['_seq'] = twig_ensure_traversable((isset($context["operadores"]) ? $context["operadores"] : $this->getContext($context, "operadores")));
         $context['_iterated'] = false;
         foreach ($context['_seq'] as $context["_key"] => $context["operador"]) {
-            // line 25
-            echo "\t\t\t<h1>";
+            // line 34
+            echo "\t\t\t\t<h1>";
             echo twig_escape_filter($this->env, $this->getAttribute((isset($context["operador"]) ? $context["operador"] : $this->getContext($context, "operador")), "nombre"), "html", null, true);
             echo "</h1>
-\t\t";
+\t\t\t";
             $context['_iterated'] = true;
         }
         if (!$context['_iterated']) {
-            // line 27
-            echo "\t\t\t<h1>No existen Operadores</h1>
-\t\t";
+            // line 36
+            echo "\t\t\t\t<h1>No existen Operadores</h1>
+\t\t\t";
         }
         $_parent = $context['_parent'];
         unset($context['_seq'], $context['_iterated'], $context['_key'], $context['operador'], $context['_parent'], $context['loop']);
         $context = array_intersect_key($context, $_parent) + $_parent;
-        // line 29
+        // line 38
         echo "\t</div>
 
 \t\t
+";
+    }
+
+    // line 43
+    public function block_javascripts($context, array $blocks = array())
+    {
+        // line 44
+        echo "\t";
+        $this->displayParentBlock("javascripts", $context, $blocks);
+        echo "
+\t<script>
+\t  \$(function (e) {
+\t  \te.preventDefault();
+\t    \$('#myTab a:last').tab('show');
+\t  })
+\t</script>
+
 ";
     }
 
@@ -101,6 +128,6 @@ class __TwigTemplate_4b8d92a58a2152928c9f40fe9768f62e extends Twig_Template
 
     public function getDebugInfo()
     {
-        return array (  86 => 29,  79 => 27,  71 => 25,  66 => 24,  59 => 20,  49 => 12,  46 => 11,  41 => 8,  38 => 7,  33 => 4,  30 => 3,);
+        return array (  106 => 44,  103 => 43,  96 => 38,  89 => 36,  81 => 34,  76 => 33,  60 => 20,  50 => 12,  47 => 11,  42 => 8,  39 => 7,  34 => 4,  31 => 3,);
     }
 }
