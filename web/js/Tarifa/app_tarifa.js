1244cb2ef3a3c9c21cad3185974e329ae6f9e18e
Raul Moctezuma <kabal@live.com.mx>
2013-11-07 18:26:32 -0600

    Comunicacion con el servidor REST desde angular. Edicion de funcionalidad en los controladores de angular.

--- web/js/Tarifa/app_tarifa.js
+++ web/js/Tarifa/app_tarifa.js
@@ -3,17 +3,18 @@
  */
 
 var tarifa_app = angular.module('tarifaApp', ['ngResource', 'timsaControllers', 'timsaServices', 'ngRoute'] );
-
+        // Cambia la configuracion de declaracion de llaves en Angular, para que no choque con twig
         tarifa_app.config(['$interpolateProvider', '$routeProvider' , function($interpolateProvider, $routeProvider){
                     $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
 
+                    // Realiza un ruteo, obteniendo la vista necesaria para el campo NgView. Y asignando el controlador.
                     $routeProvider.
                         when('/agencia/:agenciaID', {
-                            templateUrl: rutaHMTL,
+                            templateUrl: "http://localhost/controlTimsa/web/partials/tarifa_agencia.html",
                             controller: 'tarifaController'
                         }).
                         otherwise({
-                            redirectTo: '/agencias/1'
+                            redirectTo: '/agencia/1'
                         });
                 }]);
 
@@ -21,25 +22,31 @@ var timsaServices = angular.module('timsaServices', [] );
 
 timsaServices.factory('TarifaAgencia', ['$resource',
     function ($resource){
-        return $resource('../main/rest/agencia/tarifas/:agenciaID', {} ,{
-            query: { method:'GET', params:{agenciaID:'1'}, isArray:true }
+        return $resource('http://localhost/controlTimsa/web/app_dev.php/main/rest/agencia/tarifas/:agenciaID', {} ,{
+            // Sobrescribe el metdo de $resource, personalidandolo con un valor en default.
+            // los demas metodos REST de http se dejan como vienen predefinidos en $resource
+            get: { method:'GET', params:{agenciaID:'1'}, isArray: true }
         });
     }
 ]);
 
 var timsaControllers = angular.module('timsaControllers', [] );
 
-timsaControllers.controller('tarifaController', ['$scope', 'TarifaAgencia',
-                                function($scope, TarifaAgencia){
+timsaControllers.controller('tarifaController', [ '$scope', 'TarifaAgencia', '$routeParams',
+                                function($scope, TarifaAgencia, $routeParams){
 
-                                    $scope.loadImage = "http://localhost/TimsaLzc/web/images/loading.gif";
+                                    $scope.loadImage = "http://localhost/controlTimsa/web/images/loading.gif";
                                     $scope.load = false;
 
-                                    $scope.tarifas = TarifaAgencia.get({ agenciaID: $routeParams.agenciaID } ,function(){
+                                    $scope.clase = "2012";
+
+                                    $scope.setClasificacion = function(clase){
+                                        $scope.clase = clase;
+                                    }
+
+                                    $scope.tarifas = TarifaAgencia.get( { agenciaID: $routeParams.agenciaID } ,function(){
                                         $('#load').hide();
                                     });
-
                                 }
                             ]);
-
 
